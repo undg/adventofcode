0e9eb4a139738478acf359776c8a66059f43e5bf
undg <und3rdg@gmail.com>
2022-12-07 01:41:12 +0000

    2022-d5: parse stacks from file and flip them 90deg

--- 2022/5/mod.ts
+++ 2022/5/mod.ts
@@ -1,10 +1,39 @@
 export const solution = (text: string) => {
-  const partOne = undefined;
-  const partTwo = undefined;
-  return {
-    text,
-    partOne,
-    partTwo,
-  };
-};
+    const partOne = undefined
+    const partTwo = undefined
+    return {
+        text,
+        partOne,
+        partTwo,
+    }
+}
 
+export function assemblyList(text: string) {
+    const lines = text.split('\n')
+    const stackIndexEnd = lines.findIndex((line) => line.match(/^ 1.*$/))
+    const cols = lines
+        .slice(0, stackIndexEnd)
+        .map((vert) => vert.replace(/    /g, ' [ ]'))
+        .map((vert) => vert.replace(/ /g, ''))
+        .map((vert) => vert.replace(/\[/g, ''))
+        .map((vert) => vert.replace(/\]$/, ''))
+        .map((vert) => vert.replace(/\]/g, ','))
+        .map((vert) => vert.split(','))
+
+    // flip stack 90deg
+    let stacks: string[][] = [[]]
+    cols.forEach((col, idxCol) => {
+        col.forEach((value, idxRow) => {
+            if (!stacks) stacks = [[value]]
+            else if (!stacks[idxRow]) stacks[idxRow] = [value]
+            stacks[idxRow][idxCol] = value
+        })
+    })
+
+    // cleaning empty values
+    stacks.forEach((stack, idxRow) => {
+        stacks[idxRow] = stack.filter((v) => v !== '')
+    })
+
+    return stacks
+}
--- 2022/5/mod_test.ts
+++ 2022/5/mod_test.ts
@@ -1,17 +1,39 @@
-import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { solution } from "./mod.ts";
+import {
+    assertEquals,
+    assertExists,
+} from 'https://deno.land/std@0.167.0/testing/asserts.ts'
+import { assemblyList, solution } from './mod.ts'
 
-const min = await Deno.readTextFile("./min.txt");
-const input = await Deno.readTextFile("./input.txt");
+const min = await Deno.readTextFile('./min.txt')
+const input = await Deno.readTextFile('./input.txt')
 
 // solution to paste
-const { partOne, partTwo } = solution(input);
-console.log("Part One : ", partOne, "\nPart Two: ", partTwo);
+const { partOne, partTwo } = solution(input)
+console.log('Part One : ', partOne, '\nPart Two: ', partTwo)
 console.log()
 
-
-const test = solution(min);
+const test = solution(min)
 console.log(min)
-Deno.test("in->out", () => {
-  assertEquals(test.text, min);
-});
+Deno.test('in->out', () => {
+    assertEquals(test.text, min)
+})
+Deno.test({
+    name: 'should exist',
+    only: false,
+    ignore: false,
+    fn() {
+        assertExists(assemblyList(test.text))
+    },
+})
+Deno.test({
+    name: 'convert top file to list',
+    only: false,
+    ignore: false,
+    fn() {
+        assertEquals(assemblyList(test.text), [
+            ['N', 'Z'],
+            ['D', 'C', 'M'],
+            ['P'],
+        ])
+    },
+})
