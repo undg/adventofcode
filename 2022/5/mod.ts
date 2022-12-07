b14fc1259d79f5a268b4232d4451893d3c08af7c
undg <und3rdg@gmail.com>
2022-12-07 03:02:03 +0000

    2022-d5: wip

--- 2022/5/mod.ts
+++ 2022/5/mod.ts
@@ -1,5 +1,5 @@
 export const solution = (text: string) => {
-    const partOne = undefined
+    const partOne = allMoves(parseStack(text), text)
     const partTwo = undefined
     return {
         text,
@@ -8,16 +8,19 @@ export const solution = (text: string) => {
     }
 }
 
-export function assemblyList(text: string) {
+export function parseStack(text: string) {
     const lines = text.split('\n')
     const stackIndexEnd = lines.findIndex((line) => line.match(/^ 1.*$/))
     const cols = lines
         .slice(0, stackIndexEnd)
-        .map((vert) => vert.replace(/    /g, ' [ ]'))
-        .map((vert) => vert.replace(/ /g, ''))
-        .map((vert) => vert.replace(/\[/g, ''))
-        .map((vert) => vert.replace(/\]$/, ''))
-        .map((vert) => vert.replace(/\]/g, ','))
+        .map((vert) =>
+            vert
+                .replace(/ {4}/g, ' [ ]')
+                .replace(/ /g, '')
+                .replace(/\[/g, '')
+                .replace(/\]$/, '')
+                .replace(/\]/g, ',')
+        )
         .map((vert) => vert.split(','))
 
     // flip stack 90deg
@@ -36,4 +39,38 @@ export function assemblyList(text: string) {
     })
 
     return stacks
+}
+
+export function parseMove(line: string) {
+    const raw = line.split(/ /)
+    return { count: +raw[1], from: +raw[3], to: +raw[5] }
+}
+
+export function getMoves(text: string) {
+    const lines = text.split('\n')
+    return lines.filter((line) => line.match(/^move/))
+}
+
+export function createMoves(moves: string[]) {
+    return moves.map(parseMove)
+}
+
+export function move(stack: string[][], moveTxt: string) {
+    const move = parseMove(moveTxt)
+    const elToMove = stack[move.from - 1].slice(0, move.count) // save firsn n elements
+    stack[move.from - 1].splice(0, move.count) // and delete them from orig
+    stack[move.to - 1].splice(0,0,...elToMove) // and prepend them
+    return stack
+}
+
+export function allMoves(stack: string[][], text: string) {
+    const moves = getMoves(text)
+    moves.forEach(moveTxt=>{
+        move(stack, moveTxt)
+    })
+    return stack
+}
+
+export function topFromStacks(stack: string[][]) {
+    return 'dsf'
 }
--- 2022/5/mod_test.ts
+++ 2022/5/mod_test.ts
@@ -2,7 +2,16 @@ import {
     assertEquals,
     assertExists,
 } from 'https://deno.land/std@0.167.0/testing/asserts.ts'
-import { assemblyList, solution } from './mod.ts'
+import {
+    allMoves,
+    createMoves,
+    getMoves,
+    move,
+    parseMove,
+    parseStack,
+    solution,
+    topFromStacks,
+} from './mod.ts'
 
 const min = await Deno.readTextFile('./min.txt')
 const input = await Deno.readTextFile('./input.txt')
@@ -14,26 +23,115 @@ console.log()
 
 const test = solution(min)
 console.log(min)
+
 Deno.test('in->out', () => {
     assertEquals(test.text, min)
 })
+
 Deno.test({
     name: 'should exist',
     only: false,
     ignore: false,
     fn() {
-        assertExists(assemblyList(test.text))
+        assertExists(parseStack(test.text))
     },
 })
+
 Deno.test({
     name: 'convert top file to list',
     only: false,
     ignore: false,
     fn() {
-        assertEquals(assemblyList(test.text), [
+        assertEquals(parseStack(test.text), [
             ['N', 'Z'],
             ['D', 'C', 'M'],
             ['P'],
         ])
     },
 })
+
+Deno.test({
+    name: 'parse move',
+    only: false,
+    ignore: false,
+    fn() {
+        assertEquals(parseMove('move 1 from 2 to 1'), {
+            count: 1,
+            from: 2,
+            to: 1,
+        })
+    },
+})
+
+Deno.test({
+    name: 'get all moves',
+    only: false,
+    ignore: false,
+    fn() {
+        assertEquals(getMoves(test.text), [
+            'move 1 from 2 to 1',
+            'move 3 from 1 to 3',
+            'move 2 from 2 to 1',
+            'move 1 from 1 to 2',
+        ])
+    },
+})
+
+Deno.test({
+    name: 'creeate moves',
+    only: false,
+    ignore: false,
+    fn() {
+        assertEquals(createMoves(getMoves(test.text)), [
+            { count: 1, from: 2, to: 1 },
+            { count: 3, from: 1, to: 3 },
+            { count: 2, from: 2, to: 1 },
+            { count: 1, from: 1, to: 2 },
+        ])
+    },
+})
+
+Deno.test({
+    name: 'move stack',
+    only: false,
+    ignore: false,
+    fn() {
+        assertEquals(
+            move([['N', 'Z'], ['D', 'C', 'M'], ['P']], 'move 1 from 2 to 1'),
+            [['D', 'N', 'Z'], ['C', 'M'], ['P']]
+        )
+        assertEquals(
+            move([['N', 'Z'], ['D', 'C', 'M'], ['P']], 'move 2 from 1 to 3'),
+            [[], ['D', 'C', 'M'], ['N', 'Z', 'P']]
+        )
+        assertEquals(
+            move([['P', 'N', 'Z'], ['D', 'C', 'M'], []], 'move 3 from 1 to 3'),
+            [[], ['D', 'C', 'M'], ['P', 'N', 'Z']]
+        )
+        assertEquals(
+            move([[], ['D', 'C', 'M'], ['P', 'N', 'Z']], 'move 2 from 2 to 1'),
+            [['D', 'C'], ['M'], ['P', 'N', 'Z']]
+        )
+    },
+})
+
+Deno.test({
+    name: 'all moves',
+    only: false,
+    ignore: false,
+    fn() {
+        assertEquals(
+            allMoves([['N', 'Z'], ['D', 'C', 'M'], ['P']], test.text),
+            [['C'], ['M'], ['Z', 'N', 'D', 'P']]
+        )
+    },
+})
+
+// Deno.test({
+//     name: 'PartOne solution',
+//     only: false,
+//     ignore: false,
+//     fn() {
+//         assertEquals(topFromStacks([['N', 'Z'], ['D', 'C', 'M'], ['P']]), 'NDP')
+//     },
+// })
