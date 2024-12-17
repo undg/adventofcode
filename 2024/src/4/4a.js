509a60a00f46b2645eb06567094d36543e15710e
undg <und3rdg@gmail.com>
2024-12-17 00:54:32 +0000

    find xmas in line

--- 2024/src/4/4a.js
+++ 2024/src/4/4a.js
@@ -1,4 +1,4 @@
-import fs from 'node:fs'
+import fs from "node:fs"
 
 /** @param file {string} */
 export function getData(file) {
@@ -6,25 +6,50 @@ export function getData(file) {
 	return data.toString()
 }
 
-const xmas = ['X','M','A','S']
+const XMAS = "XMAS"
 
 /**
  * @param txt {string}
  * @returns {number}
  */
 export function line(txt) {
-	return 0
+	const found = denoise(txt).split(XMAS)
+	return found.length - 1
 }
 
 /**
- * @param txt {string}
+ * @param line {string}
  * @returns {string}
  */
-export function stripLine(txt) {
-	const stripped = txt
-	return stripped
+export function denoise(line) {
+	let out = ""
+	let buf = ""
+	for (let char of line) {
+		for (let cXmas of XMAS) {
+			if (cXmas !== char) {
+				continue
+			}
+
+			if (buf === XMAS) {
+				buf = "" // reset buffer
+				if (char === "X") {
+					buf += char
+					out += char
+				}
+				continue
+			}
+
+			if (buf.includes(cXmas)) {
+				continue
+			}
+
+			out += char
+			buf += char
+		}
+	}
+	return out
 }
 
 export function sol() {
-	return ''
+	return ""
 }
--- 2024/src/4/4a.test.js
+++ 2024/src/4/4a.test.js
@@ -1,33 +1,69 @@
 import { vol } from "memfs"
 import { describe, expect, it, vi } from "vitest"
 
-import { getData, line, stripLine } from "./4a"
 import { beforeEach } from "vitest"
+import { denoise, getData, line } from "./4a"
 
 vi.mock("node:fs")
 vi.mock("node:fs/promises")
 
-	beforeEach(() => {
-		vol.reset()
-	})
+beforeEach(() => {
+	vol.reset()
+})
 
-	const simpleInput = `..X...
+const simpleInput = `..X...
 .SAMX.
 .A..A.
 XMAS.S
 .X....`
 
 describe(`sol1`, () => {
-	it.only(`should return same simpleInput`, () => {
+	it(`should return same simpleInput`, () => {
 		const path = "/tmp/mock-file.txt"
 		vol.fromJSON({ [path]: simpleInput }, "/tmp")
 		expect(getData(path)).toBe(simpleInput)
 	})
 
-	it(`should found 0 XMAS`, () => {
-		expect(line("xxx")).to.eql(0)
+	describe(`line()`, () => {
+		it(`should found 0 XMAS`, () => {
+			expect(line("xxx")).toEqual(0)
+		})
+		it(`should found 1 XMAS`, () => {
+			expect(line("XMAS")).toEqual(1)
+		})
+		it(`should found 2 XMAS that are not ordered and with noise`, () => {
+			expect(line("....X.MAS.XMAS..")).toEqual(2)
+		})
+		it(`should found 2 XMAS because 3rd one is nested`, () => {
+			expect(line("....XMAS.XMA.XMAS.SS..")).toEqual(2)
+		})
 	})
-	it(`should return XMAS from dirty text`, () => {
-		expect(stripLine("BXMXXADS")).to.eql("XMAS")
+
+	describe(`denoise()`, () => {
+		it(`should remove all characters`, () => {
+			expect(denoise('na_xma5')).toEqual('')
+		})
+		it(`should keep one XMAS`, () => {
+			expect(denoise('XMAS')).toEqual('XMAS')
+		})
+		it(`should keep one XMAS and remove extra chars`, () => {
+			expect(denoise('-X__MA__S_')).toEqual('XMAS')
+		})
+		it(`should keep one XMAS and remove extra char that is one of 'XMAS'`, () => {
+			expect(denoise('XMXAS')).toEqual('XMAS')
+		})
+
+		it(`should keep two XMAS`, () => {
+			expect(denoise('XMASXMAS')).toEqual('XMASXMAS')
+		})
+		it(`should keep two XMAS and remove extra chars`, () => {
+			expect(denoise('-XM--ASXM--AS---')).toEqual('XMASXMAS')
+		})
+		it(`should keep three XMAS and remove extra chars`, () => {
+			expect(denoise('-XM-MASXMX-AS-XMAAXAS--')).toEqual('XMASXMASXMAS')
+		})
+		it(`should remove extra S`, () => {
+			expect(denoise('XMASS')).toEqual('XMAS')
+		})
 	})
 })
